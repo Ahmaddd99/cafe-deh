1f08c620ef45312efba3998a25aeb2adf6e7a154
Latief <90740340+Ahmaddd99@users.noreply.github.com>
2022-03-02 10:45:06 +0700

    Add files via upload

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.master')
+@section('content')
+<h1>
+Halo {{Auth::user()->name}}
+<h5>Selamat Datang di Cafe deh sama kamu</h5>
+<div class="col-10 col-sm-8 col-lg-6">
+        
+</div>
+  <p>up to uu</p>
+@endsection
